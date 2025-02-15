f45d9d85f2bf8c26a119d5e1fd853f914f6112a4
Ahnz <jihoon4680@gmail.com>
2025-02-15 18:36:17 +0900

    2025.02.15 jha {작업중}

---
+++ SwiftUI_Tutorial/Landmarks/Views/PageView/PageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by Jihoon on 2/15/25.
+//  Copyright © 2025 Apple. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+
+
+}
