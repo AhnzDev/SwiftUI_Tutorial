e4b75f09b416f8b2d4ce32343dcb1e671d5e9bfd
Ahnz <jihoon4680@gmail.com>
2025-02-18 08:11:08 +0900

    Finish Apple SwiftUI Tutorial

--- SwiftUI_Tutorial/Landmarks/Views/PageView/PageViewController.swift
+++ SwiftUI_Tutorial/Landmarks/Views/PageView/PageViewController.swift
@@ -11,39 +11,42 @@ import UIKit
 
 
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
-    
     var pages: [Page]
-    
+    @Binding var currentPage: Int
+
+
     func makeCoordinator() -> Coordinator {
-           Coordinator(self)
-       }
-    
+        Coordinator(self)
+    }
+
+
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
-        
-        
+        pageViewController.dataSource = context.coordinator
+        pageViewController.delegate = context.coordinator
         return pageViewController
     }
-    
-    
+
+
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+            [context.coordinator.controllers[currentPage]], direction: .forward, animated: true)
     }
-    
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+
+
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate  {
         var parent: PageViewController
         var controllers = [UIViewController]()
-        
-        
+
+
         init(_ pageViewController: PageViewController) {
             parent = pageViewController
             controllers = parent.pages.map { UIHostingController(rootView: $0) }
         }
-        
-        
+
+
         func pageViewController(
             _ pageViewController: UIPageViewController,
             viewControllerBefore viewController: UIViewController) -> UIViewController?
@@ -56,8 +59,8 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             }
             return controllers[index - 1]
         }
-        
-        
+
+
         func pageViewController(
             _ pageViewController: UIPageViewController,
             viewControllerAfter viewController: UIViewController) -> UIViewController?
@@ -70,7 +73,16 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             }
             return controllers[index + 1]
         }
+        func pageViewController(
+                   _ pageViewController: UIPageViewController,
+                   didFinishAnimating finished: Bool,
+                   previousViewControllers: [UIViewController],
+                   transitionCompleted completed: Bool) {
+                   if completed,
+                      let visibleViewController = pageViewController.viewControllers?.first,
+                      let index = controllers.firstIndex(of: visibleViewController) {
+                       parent.currentPage = index
+                   }
+               }
     }
-    
-    
 }
